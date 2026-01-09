6fe3d9f4b41327f1753888b8514e77b2217a236f
screaminface <screaminface@gmail.com>
2026-01-09 20:48:26 +0200

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Certify-app/sw.js', { scope: '/Certify-app/' })})}
